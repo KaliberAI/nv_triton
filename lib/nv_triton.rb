beb0f2ba38031f50dd24043258548bbf887c13e3
Mason Stallmo <mason@kaliberlabs.com>
2024-12-10 13:30:44 -0700

    initial commit

---
+++ lib/nv_triton.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nv_triton/version"
+
+module NvTriton
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/nv_triton_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe NvTriton do
+  it "has a version number" do
+    expect(NvTriton::VERSION).not_to be nil
+  end
+end
